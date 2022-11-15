67a1320b37f78741a05c09fc1d114d31eb766558
Kostas Kappas <kostas@pikei.io>
2022-11-15 20:12:54 +0200

    redux replace context

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
 
 import CategoriesScreen from './screens/CategoriesScreen.js';
 import MealsOverviewScreen from './screens/MealsOverviewScreen.js';
 import MealDetailScreen from './screens/MealDetailScreen.js';
 import FavoritesScreen from './screens/FavoritesScreen';
-import FavoritesContextProvider from './store/context/favorites-context.js';
+// import FavoritesContextProvider from './store/context/favorites-context';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -54,7 +56,8 @@ export default function App() {
   return (
 
     < >
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store} >
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -92,7 +95,8 @@ export default function App() {
 
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+        </Provider>
+      {/* </FavoritesContextProvider> */}
       <StatusBar style="light" />
     </>
   );
